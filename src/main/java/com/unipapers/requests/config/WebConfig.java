ee1791bf37e9d576dbba5e00e95d8d2c41c6f468
Luiz Gustav <luiz.vieira11@outlook.com>
2024-11-06 12:45:11 -0300

    Confirmando a api funcional

--- src/main/java/com/unipapers/requests/config/WebConfig.java
+++ src/main/java/com/unipapers/requests/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    //Esse é o repo certo
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*");

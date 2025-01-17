5247b28827d498d0374b901e9e7e988fa5b4379b
Luiz Gustav <luiz.vieira11@outlook.com>
2025-01-17 15:52:29 -0300

    Added Lombok

--- src/main/java/com/unipapers/requests/model/entity/Reader.java
+++ src/main/java/com/unipapers/requests/model/entity/Reader.java
@@ -1,7 +1,13 @@
 package com.unipapers.requests.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "tab_reader")
 public class Reader {
 
@@ -14,54 +20,4 @@ public class Reader {
     @Column(unique = true)
     private String email;
     private String password;
-
-    public Reader(){}
-
-    public Reader(Long id, String name, String phone, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/unipapers/requests/model/entity/Research.java
+++ src/main/java/com/unipapers/requests/model/entity/Research.java
@@ -3,9 +3,15 @@ package com.unipapers.requests.model.entity;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Base64;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "tab_research")
 public class Research {
 
@@ -18,67 +24,11 @@ public class Research {
     @Lob
     @Column(columnDefinition = "MEDIUMBLOB", unique = true)
     private byte[] blobFile;
+
     @ManyToOne
     @JoinColumn(name = "writer_id")
     private Writer writer;
 
     private String collaborators;
 
-    public Research(){}
-
-    public Research(Long id, String name, String description, byte[] blobFile, Writer writer) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.blobFile = blobFile;
-        this.writer = writer;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public byte[] getBlobFile() {
-        return blobFile;
-    }
-
-    public void setBlobFile(byte[] blobFile) {
-        this.blobFile = blobFile;
-    }
-
-    public Writer getWriter() {
-        return writer;
-    }
-
-    public void setWriter(Writer writer) {
-        this.writer = writer;
-    }
-
-    public String getCollaborators() {
-        return collaborators;
-    }
-
-    public void setCollaborators(String collaborators) {
-        this.collaborators = collaborators;
-    }
 }
